8997fb188da7302b5e4b0f3d3592a8f1396221f9
Chunhui Ren <chunhui@Chunhuis-MacBook-Pro.local>
2023-02-16 14:46:38 +0800

    .fantasticonrc.js

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -1,5 +1,9 @@
 const package = require('./package.json')
+const codepoints = require('./src/template/mapping.json');
 
+/**
+ * @type {import('fantasticon').RunnerOptions}
+ */
 module.exports = {
   name: 'term',
   inputDir: './src/icons',
@@ -7,14 +11,12 @@ module.exports = {
   fontTypes: ['ttf'],
   normalize: true,
   assetTypes: ['css', 'html'],
+  codepoints: codepoints,
   formatOptions: {
     ttf: {
       url: package.url,
       description: package.description,
       version: package.fontVersion
     }
-  },
-  codepoints: {
-
   }
 }
